28c83acf7443a47fb6c534480cd6edcd8dc5b8e6
undefinednpc <sasarehulka@gmail.com>
2022-01-03 17:14:53 +0100

    Changed the scan_tokens() functions to actually return tokens.

--- src/scanner/scanner.rs
+++ src/scanner/scanner.rs
@@ -1,11 +1,14 @@
-use crate::TokenType;
+use crate::{Token, TokenType};
 
 pub struct Scanner {}
 
 impl Scanner { 
-    pub fn scan_tokens(source: &str) -> Vec<&str> {
+    pub fn scan_tokens(source: &str) -> Vec<Token> {
         let splitted = source.split_whitespace();
-        let result: Vec<&str> = splitted.collect();
+        let mut result: Vec<Token> = Vec::new();
+        for token in splitted {
+            result.push(Token::new(TokenType::String, token.to_string(), 1));
+        }
         result
     }
 }
